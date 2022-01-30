93584c798d79c3033a4d911692c8d869abe136a9
mjagarapu <reachmjagarapu@gmail.com>
2022-01-30 17:45:43 -0500

    changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args){
         //Jenkins integration example
-        System.out.println("Example for jenkins..");
+        System.out.println("Jenkins Example");
     }
 }
